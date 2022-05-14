2d7618ff459acbd303f26e4acfbce9fa4ebc75ee
Joy Liu <joyliu.q@gmail.com>
2022-05-13 17:57:08 -0700

    :art: Refactor

--- src/components/useSelect.tsx
+++ src/components/useSelect.tsx
@@ -1,11 +1,8 @@
-import { useClickable } from "@chakra-ui/clickable"
 import { createDescendantContext } from "@chakra-ui/descendant"
 import {
-  useControllableState,
   useDisclosure,
   UseDisclosureProps,
   useFocusOnHide,
-  useId,
   useIds,
   useOutsideClick,
   useShortcut,
@@ -17,26 +14,20 @@ import { usePopper, UsePopperProps } from "@chakra-ui/popper"
 import {
   createContext,
   EventKeyMap,
-  getValidChildren,
   mergeRefs,
 } from "@chakra-ui/react-utils"
 import {
-  addItem,
   callAllHandlers,
   dataAttr,
   determineLazyBehavior,
   focus,
   getNextItemFromSearch,
   getOwnerDocument,
-  isActiveElement,
-  isArray,
-  isHTMLElement,
-  isString,
   LazyBehavior,
   normalizeEventKey,
-  removeItem,
 } from "@chakra-ui/utils"
 import * as React from "react"
+import { isTargetSelectItem } from "./useSelectItem"
 
 /* -------------------------------------------------------------------------------------------------
  * Create context to track descendants and their indices
@@ -357,14 +348,6 @@ export function useSelectButton(
   }
 }
 
-function isTargetSelectItem(target: EventTarget | null) {
-  // this will catch `selectitem`, `selectitemradio`, `selectitemcheckbox`
-  return (
-    isHTMLElement(target) &&
-    !!target.getAttribute("role")?.startsWith("selectitem")
-  )
-}
-
 /* -------------------------------------------------------------------------------------------------
  * useSelectList
  * -----------------------------------------------------------------------------------------------*/
@@ -510,262 +493,6 @@ export function useSelectPositioner(props: any = {}) {
       ...props.style,
     },
   })
-}
-
-/* -------------------------------------------------------------------------------------------------
- * useSelectItem: Hook for each select item within the select list.
-   We also use it in `useSelectItemOption`
- * -----------------------------------------------------------------------------------------------*/
-
-export interface UseSelectItemProps
-  extends Omit<React.HTMLAttributes<Element>, "color" | "disabled"> {
-  /**
-   * If `true`, the selectitem will be disabled
-   */
-  isDisabled?: boolean
-  /**
-   * If `true` and the selectitem is disabled, it'll
-   * remain keyboard-focusable
-   */
-  isFocusable?: boolean
-  /**
-   * Overrides the parent select's `closeOnSelect` prop.
-   */
-  closeOnSelect?: boolean
-  /**
-   * The type of the selectitem.
-   */
-  type?: React.ButtonHTMLAttributes<HTMLButtonElement>["type"]
-}
-
-export function useSelectItem(
-  props: UseSelectItemProps = {},
-  externalRef: React.Ref<any> = null,
-) {
-  const {
-    onMouseEnter: onMouseEnterProp,
-    onMouseMove: onMouseMoveProp,
-    onMouseLeave: onMouseLeaveProp,
-    onClick: onClickProp,
-    isDisabled,
-    isFocusable,
-    closeOnSelect,
-    type: typeProp,
-    ...htmlProps
-  } = props
-
-  const select = useSelectContext()
-
-  const {
-    setFocusedIndex,
-    focusedIndex,
-    closeOnSelect: selectCloseOnSelect,
-    onClose,
-    selectRef,
-    isOpen,
-    selectId,
-  } = select
-
-  const ref = React.useRef<HTMLDivElement>(null)
-  const id = `${selectId}-selectitem-${useId()}`
-
-  /**
-   * Register the selectitem's node into the domContext
-   */
-  const { index, register } = useSelectDescendant({
-    disabled: isDisabled && !isFocusable,
-  })
-
-  const onMouseEnter = React.useCallback(
-    (event: any) => {
-      onMouseEnterProp?.(event)
-      if (isDisabled) return
-      setFocusedIndex(index)
-    },
-    [setFocusedIndex, index, isDisabled, onMouseEnterProp],
-  )
-
-  const onMouseMove = React.useCallback(
-    (event: any) => {
-      onMouseMoveProp?.(event)
-      if (ref.current && !isActiveElement(ref.current)) {
-        onMouseEnter(event)
-      }
-    },
-    [onMouseEnter, onMouseMoveProp],
-  )
-
-  const onMouseLeave = React.useCallback(
-    (event: any) => {
-      onMouseLeaveProp?.(event)
-      if (isDisabled) return
-      setFocusedIndex(-1)
-    },
-    [setFocusedIndex, isDisabled, onMouseLeaveProp],
-  )
-
-  const onClick = React.useCallback(
-    (event: React.MouseEvent) => {
-      onClickProp?.(event)
-      if (!isTargetSelectItem(event.currentTarget)) return
-      /**
-       * Close select and parent selects, allowing the SelectItem
-       * to override its parent select's `closeOnSelect` prop.
-       */
-      if (closeOnSelect ?? selectCloseOnSelect) {
-        onClose()
-      }
-    },
-    [onClose, onClickProp, selectCloseOnSelect, closeOnSelect],
-  )
-
-  const isFocused = index === focusedIndex
-
-  const trulyDisabled = isDisabled && !isFocusable
-
-  useUpdateEffect(() => {
-    if (!isOpen) return
-    if (isFocused && !trulyDisabled && ref.current) {
-      focus(ref.current, {
-        nextTick: true,
-        selectTextIfInput: false,
-        preventScroll: false,
-      })
-    } else if (selectRef.current && !isActiveElement(selectRef.current)) {
-      focus(selectRef.current, { preventScroll: false })
-    }
-  }, [isFocused, trulyDisabled, selectRef, isOpen])
-
-  const clickableProps = useClickable({
-    onClick,
-    onMouseEnter,
-    onMouseMove,
-    onMouseLeave,
-    ref: mergeRefs(register, ref, externalRef),
-    isDisabled,
-    isFocusable,
-  })
-
-  return {
-    ...htmlProps,
-    ...clickableProps,
-    type: typeProp ?? (clickableProps as any).type,
-    id,
-    role: "selectitem",
-    tabIndex: isFocused ? 0 : -1,
-  }
-}
-
-/* -------------------------------------------------------------------------------------------------
- * useSelectOption: Composes useSelectItem to provide a selectable/checkable select item
- * -----------------------------------------------------------------------------------------------*/
-
-export interface UseSelectOptionOptions {
-  value?: string
-  isChecked?: boolean
-  type?: "radio" | "checkbox"
-  children?: React.ReactNode
-}
-
-export interface UseSelectOptionProps
-  extends Omit<UseSelectItemProps, "type">,
-    UseSelectOptionOptions {}
-
-export function useSelectOption(
-  props: UseSelectOptionProps = {},
-  ref: React.Ref<any> = null,
-) {
-  const { type = "radio", isChecked, ...rest } = props
-  const ownProps = useSelectItem(rest, ref)
-  return {
-    ...ownProps,
-    role: `selectitem${type}`,
-    "aria-checked": isChecked as React.AriaAttributes["aria-checked"],
-  }
-}
-
-/* -------------------------------------------------------------------------------------------------
- * useSelectOptionGroup: Manages the state of multiple selectable selectitem or select option
- * -----------------------------------------------------------------------------------------------*/
-
-export interface UseSelectOptionGroupProps {
-  value?: string | string[]
-  defaultValue?: string | string[]
-  type?: "radio" | "checkbox"
-  onChange?: (value: string | string[]) => void
-  children?: React.ReactNode
-}
-
-export function useSelectOptionGroup(props: UseSelectOptionGroupProps = {}) {
-  const {
-    children,
-    type = "radio",
-    value: valueProp,
-    defaultValue,
-    onChange: onChangeProp,
-    ...htmlProps
-  } = props
-
-  const isRadio = type === "radio"
-
-  const fallback = isRadio ? "" : []
-
-  const [value, setValue] = useControllableState({
-    defaultValue: defaultValue ?? fallback,
-    value: valueProp,
-    onChange: onChangeProp,
-  })
-
-  const onChange = React.useCallback(
-    (selectedValue: string) => {
-      if (type === "radio" && isString(value)) {
-        setValue(selectedValue)
-      }
-
-      if (type === "checkbox" && isArray(value)) {
-        const nextValue = value.includes(selectedValue)
-          ? removeItem(value, selectedValue)
-          : addItem(value, selectedValue)
-
-        setValue(nextValue)
-      }
-    },
-    [value, setValue, type],
-  )
-
-  const validChildren = getValidChildren(children)
-
-  const clones = validChildren.map((child) => {
-    /**
-     * We've added an internal `id` to each `SelectItemOption`,
-     * let's use that for type-checking.
-     *
-     * We can't rely on displayName or the element's type since
-     * they can be changed by the user.
-     */
-    if ((child.type as any).id !== "SelectItemOption") return child
-
-    const onClick = (event: MouseEvent) => {
-      onChange(child.props.value)
-      child.props.onClick?.(event)
-    }
-
-    const isChecked =
-      type === "radio"
-        ? child.props.value === value
-        : value.includes(child.props.value)
-
-    return React.cloneElement(child, {
-      type,
-      onClick,
-      isChecked,
-    })
-  })
-
-  return {
-    ...htmlProps,
-    children: clones,
-  }
 }
 
 export function useSelectState() {
