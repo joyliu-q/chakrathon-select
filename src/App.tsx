99ff31440ddb30983020439161ba27f17333811b
Jinay Jain <jinaybjain@gmail.com>
2022-05-13 20:58:52 -0400

    Delete boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css'
+import { Select } from "./components";
 
 function App() {
   return (
-   <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Select>
+      <option value="">Select</option>
+      <option value="1">One</option>
+    </Select>
   );
 }
 
