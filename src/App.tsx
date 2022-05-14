886cb6389d09d67c9e92bf068e70a139ae4342cf
Jinay Jain <jinaybjain@gmail.com>
2022-05-13 21:08:48 -0400

    Add ChakraProvider to  App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { ChakraProvider, Box } from "@chakra-ui/react";
+
 import { Select } from "./components";
 
 function App() {
   return (
-    <Select>
-      <option value="">Select</option>
-      <option value="1">One</option>
-    </Select>
+    <ChakraProvider>
+      <Box m="auto" maxW="300px" maxH="300px">
+        <Select>
+          <option value="">Select</option>
+          <option value="1">One</option>
+        </Select>
+      </Box>
+    </ChakraProvider>
   );
 }
 
