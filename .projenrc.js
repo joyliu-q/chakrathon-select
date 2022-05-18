f21c889b1a73a4d6e6706f00c77d54ddfeab451f
daniel-moon32 <dm32@illinois.edu>
2022-05-17 18:11:58 -0700

    add dependencies

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,20 @@ const project = new web.ReactTypeScriptProject({
   defaultReleaseBranch: "main",
   name: "chakrathon-select",
 
-  // deps: [],                /* Runtime dependencies of this module. */
+  deps: ["@chakra-ui/clickable",
+    "@chakra-ui/descendant",
+    "@chakra-ui/form-control",
+    "@chakra-ui/popper",
+    "@chakra-ui/react",
+    "@chakra-ui/system",
+    "@emotion/react",
+    "@emotion/styled",
+    "edit-distance",
+    "framer-motion",
+    "react",
+    "react-dom",
+    "react-scripts",
+    "web-vitals"],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
