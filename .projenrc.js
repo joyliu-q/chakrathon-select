02f3b7ad1a7ec6903920c196b03535e0774c2469
daniel-moon32 <dm32@illinois.edu>
2022-05-17 18:19:14 -0700

    update dependencies

--- .projenrc.js
+++ .projenrc.js
@@ -19,6 +19,6 @@ const project = new web.ReactTypeScriptProject({
     "web-vitals"],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  packageName: "chakrathon-select",  /* The "name" in package.json. */
 });
 project.synth();
